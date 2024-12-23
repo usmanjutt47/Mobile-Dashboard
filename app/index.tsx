339383ed9df4dbd27ec93a4e6f23cc926379f9fa
Muhammad Usman <142773789+usmanjutt47@users.noreply.github.com>
2024-12-23 22:35:04 +0500

    Update index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -5,29 +5,55 @@ import {
   StatusBar,
   Image,
   TouchableOpacity,
+  Dimensions,
+  ScrollView,
 } from "react-native";
 import React from "react";
+import { useNavigation } from "@react-navigation/native";
 
-export default function Dashboard({ navigation }: any) {
+const { width, height } = Dimensions.get("window");
+
+export default function Dashboard() {
+  const navigation = useNavigation();
   return (
     <View style={styles.container}>
       <StatusBar barStyle={"dark-content"} backgroundColor={"#F5F8FF"} />
-      <View style={styles.innerContainer}>
-        <View style={styles.header}>
-          <Image
-            source={require("../assets/images/openDrawer.png")}
-            style={styles.openDrawerImage}
-          />
+      <ScrollView contentContainerStyle={styles.scrollViewContent}>
+        <View style={styles.innerContainer}>
+          <TouchableOpacity>
+            <Image
+              source={require("../assets/images/openDrawer.png")}
+              style={styles.openDrawerImage}
+            />
+          </TouchableOpacity>
+
+          <Text style={styles.headingText}>Dashboard</Text>
+          <TouchableOpacity style={styles.TotalEmployeeContainer}>
+            <View style={styles.TotalEmployeeInnerContainer}>
+              <View style={{ flexDirection: "row" }}>
+                <Image
+                  source={require("../assets/images/men.png")}
+                  style={styles.TotalEmployeeImage}
+                />
+                <Text style={styles.employeeText}>Total Employees</Text>
+              </View>
+              <Text style={styles.employeeTextPrice}>6.6k</Text>
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.TotalApplicationContainer}>
+            <View style={styles.TotalEmployeeInnerContainer}>
+              <View style={{ flexDirection: "row" }}>
+                <Image
+                  source={require("../assets/images/applications.png")}
+                  style={styles.TotalEmployeeImage}
+                />
+                <Text style={styles.applicationText}>Leave Applications</Text>
+              </View>
+              <Text style={styles.applicationTextPrice}>114</Text>
+            </View>
+          </TouchableOpacity>
         </View>
-        <Text style={styles.headingText}>Dashboard</Text>
-        <TouchableOpacity style={styles.TotalEmployeeContainer}>
-          <Image
-            source={require("../assets/images/men.png")}
-            style={styles.openDrawerImage}
-          />
-          <Text />
-        </TouchableOpacity>
-      </View>
+      </ScrollView>
     </View>
   );
 }
@@ -37,34 +63,82 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#F5F8FF",
   },
+  scrollViewContent: {
+    flexGrow: 1,
+  },
   innerContainer: {
-    height: "100%",
     width: "90%",
-    alignSelf: "center",
     backgroundColor: "#F5F8FF",
+    alignSelf: "center",
   },
   header: {
     width: "100%",
+    alignItems: "flex-start",
   },
   openDrawerImage: {
-    width: 30,
-    height: 30,
+    width: width * 0.08,
+    height: width * 0.08,
     resizeMode: "contain",
   },
   headingText: {
-    fontSize: 31,
+    fontSize: width * 0.08,
     fontWeight: "bold",
-    marginTop: "10%",
+    marginTop: height * 0.02,
   },
   TotalEmployeeContainer: {
     width: "100%",
-    height: 84,
+    height: height * 0.1,
     borderRadius: 23,
     backgroundColor: "#FFFFFF",
     elevation: 2,
-    marginTop: "10%",
+    marginTop: height * 0.03,
     justifyContent: "center",
     paddingLeft: "5%",
     paddingRight: "5%",
+  },
+  TotalEmployeeImage: {
+    width: width * 0.08,
+    height: width * 0.08,
+    resizeMode: "contain",
+  },
+  TotalEmployeeInnerContainer: {
+    height: "100%",
+    width: "100%",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  employeeText: {
+    fontSize: 16,
+    fontWeight: "bold",
+    width: "50%",
+    marginLeft: "3%",
+  },
+  employeeTextPrice: {
+    fontWeight: "bold",
+    fontSize: 20,
+    color: "#624AB3",
+  },
+  TotalApplicationContainer: {
+    width: "100%",
+    height: height * 0.1,
+    borderRadius: 23,
+    backgroundColor: "#FFFFFF",
+    elevation: 2,
+    marginTop: height * 0.01,
+    justifyContent: "center",
+    paddingLeft: "5%",
+    paddingRight: "5%",
+  },
+  applicationText: {
+    fontSize: 16,
+    fontWeight: "bold",
+    width: "50%",
+    marginLeft: "3%",
+  },
+  applicationTextPrice: {
+    fontWeight: "bold",
+    fontSize: 20,
+    color: "#FF7648",
   },
 });
